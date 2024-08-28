3126c2ea36f7b8146e4bb85600ea218b83a1f62c
imannurwahyu <133605835+imannurwahyu@users.noreply.github.com>
2024-08-28 12:57:07 +0700

    Add files via upload

---
+++ DemoNilai.java
@@ -0,0 +1,14 @@
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai n1 = new Nilai();
+        n1.nama = "IMAN";
+        n1.NIM = 23090;
+        n1.nilaiA = 80;
+        n1.nilaiT = 90;
+        n1.nilaiUT = 70;
+        n1.nilaiUA = 80;
+
+        n1.nilai();
+        n1.cetakNilai();
+    }
+}
