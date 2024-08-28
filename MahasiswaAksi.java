3d5a7fb80fa3aaaf414dc771898ce5328bd5c598
imannurwahyu <133605835+imannurwahyu@users.noreply.github.com>
2024-08-28 12:56:18 +0700

    Delete MahasiswaAksi.java

--- MahasiswaAksi.java
+++
@@ -1,9 +0,0 @@
-public class MahasiswaAksi {
-    public static void main(String[] args) {
-        Mahasiswa m1 = new Mahasiswa();
-        m1.nama = "iman";
-        m1.membaca();
-        m1.nyontek();
-        m1.modifikasi();
-    }
-}
